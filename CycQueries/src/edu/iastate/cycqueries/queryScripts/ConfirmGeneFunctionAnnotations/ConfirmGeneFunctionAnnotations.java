a9bde7fd0f7d31de44dd90572bc307bf3dedf09c
Jesse Walsh <jrwalsh@iastate.edu>
2016-06-14 16:55:52 -0500

    The original requirements for this program changed as follows:
    - comparison will aggregate proteins into the gene level and compare
    there
    
    - requires a method to aggregate the merged UniProt and BRENDA EC
    annotations by removing duplicates horizontally (i.e. between UniProt
    and BRENDA) and vertically (i.e. between alternate isoforms of the same
    gene)
    
    - if a gene is not found in the database (CornCyc/MaizeCyc) it is
    considered a single False Negative per experimental annotation
    
    - now uses the merged annotation file as input

--- CycQueries/src/edu/iastate/cycqueries/queryScripts/ConfirmGeneFunctionAnnotations/ConfirmGeneFunctionAnnotations.java
+++ CycQueries/src/edu/iastate/cycqueries/queryScripts/ConfirmGeneFunctionAnnotations/ConfirmGeneFunctionAnnotations.java
@@ -28,18 +28,25 @@ import edu.iastate.javacyco.Reaction;
  * !! The search criteria for MaizeCyc differs from CornCyc, as MaizeCyc does not include accurate splice information, the _P## will not match, and also
  * MaizeCyc does not have any citation data in the Enzymatic Reactions.  The code was manually modified to account for these two discrepancies.
  * 
+ * Take in a list of gene model name to EC pairs (with additional columns of info) and
+ * 1) search gene model name to resolve into a gene
+ * 2) get products of gene
+ * 3) get catalyzes of product (i.e. enzymatic reactions)
+ * 4) print citations for the enzymatic reactions
+ * 5) get reaction of enzymatic reaction
+ * 6) print EC number of reaction
  * @throws PtoolsErrorException
  * @author Jesse Walsh 5/31/2016
  */
 public class ConfirmGeneFunctionAnnotations {
-	private static String hostCorn = "jrwalsh.student.iastate.edu";
-	private static String hostMaize = "jrwalsh-server.student.iastate.edu";
+	private static String hostCorn = "jrwalsh";
+	private static String hostMaize = "jrwalsh-server";
 	private static String organismCorn = "CORN";
 	private static String organismMaize = "MAIZE";
 	private static int port = 4444;
 	private static String frameType = "|All-Genes|";
-	private static String inFile = "C:\\Users\\Jesse\\workspace\\CycQueries\\bin\\edu\\iastate\\cycqueries\\queryScripts\\ConfirmGeneFunctionAnnotations\\blast-results-fixed";
-	//We expect this inFile to be in the format 
+	private static String goldStandardECAnnotations = "C:\\Users\\Jesse\\git\\CycQueries\\CycQueries\\bin\\edu\\iastate\\cycqueries\\queryScripts\\ConfirmGeneFunctionAnnotations\\gold-standard-gene-EC-assignments.tab";
+	//We expect the goldStandardECAnnotations to be in the following format 
 	//Protein \t EC-Numbers
 	//Where protein is the blast hit result which represents a corn gene model name and is used as the query term
 	//and EC-Numbers is a semi-colon delimited list of EC numbers uniprot associates with this protein
@@ -48,10 +55,8 @@ public class ConfirmGeneFunctionAnnotations {
 		try {
 			Long start = System.currentTimeMillis();
 			
-			listEVCodes(hostCorn, organismCorn);
-//			listEVCodes(hostMaize, organismMaize);
-			
-//			listEVCodesAllowMultipleGeneHits(hostCorn, organismCorn);
+			calculateECAccuracy(hostCorn, organismCorn, true, true);
+			calculateECAccuracy(hostMaize, organismMaize, true, true);
 			
 			Long stop = System.currentTimeMillis();
 			Long runtime = (stop - start) / 1000;
@@ -64,22 +69,69 @@ public class ConfirmGeneFunctionAnnotations {
 		
 	}
 	
-	/**
-	 * Take in a list of gene model name to EC pairs (with additional columns of info) and
-	 * 1) search gene model name to resolve into a gene
-	 * 2) get products of gene
-	 * 3) get catalyzes of product (i.e. enzymatic reactions)
-	 * 4) print citations for the enzymatic reactions
-	 * 5) get reaction of enzymatic reaction
-	 * 6) print EC number of reaction
-	 * 
-	 * @throws PtoolsErrorException
-	 */
-	private static void listEVCodes(String host, String organism) throws PtoolsErrorException {
+	private static void printGeneECNumbers(String host, String organism) throws PtoolsErrorException {
 		JavacycConnection conn = new JavacycConnection(host, port);
-		System.out.println("Confirm EV codes for Blast Results: " + organism);
-		System.out.println("ProteinFrameID\tEC-Number");
 		conn.selectOrganism(organism);
+		
+		BufferedReader reader = null;
+		String line = null;
+		try {
+			reader = new BufferedReader(new FileReader("C:\\Users\\Jesse\\git\\CycQueries\\CycQueries\\bin\\edu\\iastate\\cycqueries\\queryScripts\\ConfirmGeneFunctionAnnotations\\temp.tab"));
+			
+			while ((line = reader.readLine()) != null) {
+				String[] data = line.split("\t");
+				String item = data[0];
+				
+				//Resolve provided protein ID to gene object(s) from selected database
+				ArrayList<Gene> genes = getGeneMatch(conn, item);//Merge all transcripts' EC annotations into the gene model
+				
+				//Process EC numbers from matched objects
+				if (genes == null || genes.isEmpty()) {
+					System.err.println("No match for " + item);
+					continue;
+				} else {
+					for (Gene gene : genes) {
+						TreeSet<String> ECList = getGeneLevelECList(conn, gene);
+						if (ECList == null || ECList.isEmpty()) {
+							System.out.println(item + "\t" + gene.getCommonName() + "\t");
+						} else {
+							System.out.print(item + "\t" + gene.getCommonName() + "\t");
+							for (String EC : ECList) {
+								System.out.print(EC + ";");
+							}
+							System.out.println();
+						}
+					}
+				}
+			}
+		} catch (FileNotFoundException exception) {
+			exception.printStackTrace();
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		finally {
+			try {
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			} catch (IOException exception) {
+				exception.printStackTrace();
+			}
+		}
+	}
+	
+	private static void calculateECAccuracy(String host, String organism, boolean geneComparison, boolean countMissingAsFN) throws PtoolsErrorException {
+		JavacycConnection conn = new JavacycConnection(host, port);
+		conn.selectOrganism(organism);
+		
+		System.out.println("Calculate EC accuracy of annotations in organism " + organism + " against known true annotations determined at the " + (geneComparison ? "gene" : "transcript") + " level");
+		System.out.println("ProteinFrameID\tExperimental EC-Number\tComputational EC-Number\tType");
 		
 		int TP = 0;//When the uniprot ec matches the cyc database EC
 		int FP = 0;//When the cyc database has an EC that is not in the Uniprot EC list
@@ -89,42 +141,69 @@ public class ConfirmGeneFunctionAnnotations {
 		BufferedReader reader = null;
 		String line = null;
 		try {
-			reader = new BufferedReader(new FileReader(inFile));
-			reader.readLine();//Clear header line
+			reader = new BufferedReader(new FileReader(goldStandardECAnnotations));
+			reader.readLine();//Ignore header line
 			
 			while ((line = reader.readLine()) != null) {
 				String[] data = line.split("\t");
 				String item = data[0];
 				TreeSet<String> UniProtECs = new TreeSet<String>();
-				for (String EC : data[1].split("; ")) {
+				for (String EC : data[1].split(";")) {
 					UniProtECs.add(EC);
 				}
-
-				Gene gene = getGeneMatch(conn, item);//Find the gene which matches the uniprot blast hit
-				if (gene == null) {
-					System.err.println("No gene match for " + item);
+				
+				//Resolve provided protein ID to gene object(s) from selected database
+				ArrayList<Gene> genes;
+				if (geneComparison) genes = getGeneMatch(conn, item);//Merge all transcripts' EC annotations into the gene model
+				else genes = getTranscriptMatch(conn, item);//Must match the _P##, _FGP### suffix.  Treat _P/_T and _FGP/_FGT as interchangable
+				
+				//Process EC numbers from matched objects
+				if (genes == null || genes.isEmpty()) {
+					if (countMissingAsFN) {
+						for (String uniprotEC : UniProtECs) {
+							FN++;
+							System.out.println(item + "\t" + uniprotEC + "\t" + "" + "\t*FN");
+						}
+					} else {
+						System.err.println("No match for " + item);
+					}
 					continue;
 				} else {
-					TreeSet<String> ECList = getECList(conn, gene);
-
-					//Print the list of EC values for the cyc database
-					if (ECList == null || ECList.isEmpty()) {
-						System.out.println(gene.getLocalID() + "\t");
-					} else {
-						for (String EC : ECList) {
-							System.out.println(gene.getLocalID() + "\t" + EC);
+					TreeSet<String> ECList = new TreeSet<String>();
+					for (Gene gene : genes) {
+						TreeSet<String> temp = getGeneLevelECList(conn, gene);
+						if (temp != null && !temp.isEmpty()) {
+							ECList.addAll(temp);
 						}
 					}
+
+//					//Print the list of EC values for the cyc database
+//					if (ECList == null || ECList.isEmpty()) {
+//						System.out.println(item + "\t");
+//					} else {
+//						for (String EC : ECList) {
+//							System.out.println(item + "\t" + EC);
+//						}
+//					}
 					
 					//Calculate TP,FP,FN
 					for (String uniprotEC : UniProtECs) {
-						if (ECList.contains(uniprotEC)) TP++;
-						else FN++;
+						if (ECList.contains(uniprotEC)) {
+							TP++;
+							System.out.println(item + "\t" + uniprotEC + "\t" + uniprotEC + "\tTP");
+						}
+						else {
+							FN++;
+							System.out.println(item + "\t" + uniprotEC + "\t" + "" + "\tFN");
+						}
 					}
 					for (String predictedEC : ECList) {
 						if (UniProtECs.contains(predictedEC)) {
 							//ignore, already counted TPs
-						} else FP++;
+						} else {
+							FP++;
+							System.out.println(item + "\t" + "" + "\t" + predictedEC + "\tFP");
+						}
 					}
 				}
 			}
@@ -155,10 +234,12 @@ public class ConfirmGeneFunctionAnnotations {
 		}
 	}
 	
-	private static Gene getGeneMatch(JavacycConnection conn, String query) throws PtoolsErrorException {
+	private static ArrayList<Gene> getTranscriptMatch(JavacycConnection conn, String query) throws PtoolsErrorException {
+		ArrayList<Gene> genes = new ArrayList<Gene>();
 		ArrayList<Frame> list = conn.search(query, frameType); // Search for gene
 		if (list.size() == 1) {
-			return (Gene) list.get(0);
+			genes.add((Gene) list.get(0));
+			return genes;
 		} else if (list.size() == 0) {
 			//Try to replace any _P## with an _T##, common error when searching CornCyc/MaizeCyc
 			String p = "(.*)(_P)(\\d\\d$)"; // 3 capture groups.  first group is any length of letters or numbers, followed by an _P, followed by 2 numbers and an end of line
@@ -178,107 +259,14 @@ public class ConfirmGeneFunctionAnnotations {
 			}
 			list = conn.search(query, frameType); // Search for gene using modified query
 			if (list.size() == 1) {
-				return (Gene) list.get(0);
+				genes.add((Gene) list.get(0));
+				return genes;
 			}
 		}
 		return null; //Search fails if it finds more than 1 hit on initial search, or if it finds 0 or more than 1 hit on modified search
 	}
 	
-	/**
-	 * Similar to above method, but if the search returns multiple hits (i.e. splice variants), merge them all together (i.e. do comparison at the gene level, not the
-	 * splice variant level).  To do this, remove the _P##, _T##, _FGP####, and _FGT####.
-	 * @param host
-	 * @param organism
-	 * @throws PtoolsErrorException
-	 */
-	private static void listEVCodesAllowMultipleGeneHits(String host, String organism) throws PtoolsErrorException {
-		JavacycConnection conn = new JavacycConnection(host, port);
-		System.out.println("Confirm EV codes for Blast Results, merge splice variants: " + organism);
-		System.out.println("ProteinFrameID\tEC-Number");
-		conn.selectOrganism(organism);
-		
-		int TP = 0;//When the uniprot ec matches the cyc database EC
-		int FP = 0;//When the cyc database has an EC that is not in the Uniprot EC list
-		int FN = 0;//When Uniprot has an EC that is not in the cyc database EC list
-		int TN = 0;//We cannot capture this value with the data we have
-		
-		BufferedReader reader = null;
-		String line = null;
-		try {
-			reader = new BufferedReader(new FileReader(inFile));
-			reader.readLine();//Clear header line
-			
-			while ((line = reader.readLine()) != null) {
-				String[] data = line.split("\t");
-				String item = data[0];
-				TreeSet<String> UniProtECs = new TreeSet<String>();
-				for (String EC : data[1].split("; ")) {
-					UniProtECs.add(EC);
-				}
-
-				ArrayList<Gene> genes = getGeneMatchMultiple(conn, item);//Find the gene which matches the uniprot blast hit
-				if (genes == null || genes.isEmpty()) {
-					System.err.println("No gene match for " + item);
-					continue;
-				} else {
-//					System.out.println(item + " matched up with " + genes.size() + " hits");
-					TreeSet<String> ECList = new TreeSet<String>();
-					for (Gene gene : genes) {
-						TreeSet<String> temp = getECList(conn, gene);
-						if (temp != null && !temp.isEmpty()) {
-							ECList.addAll(temp);
-						}
-					}
-
-					//Print the list of EC values for the cyc database
-					if (ECList == null || ECList.isEmpty()) {
-						System.out.println(item + "\t");
-					} else {
-						for (String EC : ECList) {
-							System.out.println(item + "\t" + EC);
-						}
-					}
-					
-					//Calculate TP,FP,FN
-					for (String uniprotEC : UniProtECs) {
-						if (ECList.contains(uniprotEC)) TP++;
-						else FN++;
-					}
-					for (String predictedEC : ECList) {
-						if (UniProtECs.contains(predictedEC)) {
-							//ignore, already counted TPs
-						} else FP++;
-					}
-				}
-			}
-			
-			System.out.println("TP = " + TP);
-			System.out.println("FP = " + FP);
-			System.out.println("FN = " + FN);
-			System.out.println("TN cannot be determined");
-		} catch (FileNotFoundException exception) {
-			exception.printStackTrace();
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		} catch (Exception exception) {
-			exception.printStackTrace();
-		}
-		finally {
-			try {
-			} catch (Exception exception) {
-				exception.printStackTrace();
-			}
-			try {
-				if (reader != null) {
-					reader.close();
-				}
-			} catch (IOException exception) {
-				exception.printStackTrace();
-			}
-		}
-	}
-	
-	private static ArrayList<Gene> getGeneMatchMultiple(JavacycConnection conn, String query) throws PtoolsErrorException {
+	private static ArrayList<Gene> getGeneMatch(JavacycConnection conn, String query) throws PtoolsErrorException {
 		ArrayList<Gene> genes = new ArrayList<Gene>();
 		
 		String p = "(.*)(_P)(\\d\\d$)"; // 3 capture groups.  first group is any length of letters or numbers, followed by an _P, followed by 2 numbers and an end of line
@@ -304,45 +292,50 @@ public class ConfirmGeneFunctionAnnotations {
 		return null;
 	}
 	
-	private static TreeSet<String> getECList(JavacycConnection conn, Gene gene) throws PtoolsErrorException {
+	// Takes in a list of genes or transcripts and reports all the ec numbers for every isoform of that gene
+	private static TreeSet<String> getGeneLevelECList(JavacycConnection conn, Gene gene) throws PtoolsErrorException {
 		TreeSet<String> ECList = new TreeSet<String>();
 		ArrayList<Protein> proteins = gene.getProducts();//Genes can have multiple proteins, and rarely no proteins or non-protein products
 		if (proteins.size() == 0) {
 			System.err.println("No protein");
 			return null;
 		}
-		else if (proteins.size() > 1) {
-			System.err.println("Too many proteins");
-			return null;
-		}
+//		else if (proteins.size() > 1) {
+//			System.err.println("Too many proteins");
+//			return null;
+//		}
 		else {
-			Protein protein = proteins.get(0);
-			for (Frame catalysis : protein.getCatalysis()) {
-				boolean computationalEVCode = false;
-//				boolean computationalEVCode = true; //MaizeCyc is all computational, and does not included it explicitly
-				for (Object citation : catalysis.getSlotValues("CITATIONS")) {
-					if (citation.toString().contains("EV-COMP")) { //We don't care how many citations, as long as one of them indicates the EC was computationally predicted
-						computationalEVCode = true;
-					}
-				}
-				if (computationalEVCode) {
-					Reaction reaction = (Reaction) Reaction.load(conn, catalysis.getSlotValue("REACTION"));
-					String EC = null;
-					if (reaction != null) EC = reaction.getEC();
-					if (EC != null && EC.length() > 0) {
-						EC = EC.replace("EC-", "");//Don't allow partial EC numbers, such as 1.-.-.- or 1.2.3.-
-						String p = "\\d{1,3}?.\\d{1,3}?.\\d{1,3}?.\\d{1,3}?";//EC numbers are, like IP addresses, 4 numbers of up to 3 digits each, separated by periods
-						Pattern r = Pattern.compile(p);
-						Matcher m = r.matcher(EC);
-						if (m.find()) {
-							ECList.add(EC);
-						} else {
-							System.err.println("partial EC? : " + EC);
+			for(Protein protein : proteins) {
+				for (Frame catalysis : protein.getCatalysis()) {
+					boolean computationalEVCode = false;
+					ArrayList<String> citations = catalysis.getSlotValues("CITATIONS");
+					if (citations == null || citations.isEmpty()) computationalEVCode = true; //MaizeCyc is all computational, and does not included it explicitly
+					else {
+						for (String citation : citations) {
+							if (citation.toString().contains("EV-COMP")) { //We don't care how many citations, as long as one of them indicates the EC was computationally predicted
+								computationalEVCode = true;
+							}
 						}
-						
 					}
-				} else {
-					System.err.println("Failed to find computational citation for :" + gene.getLocalID());
+					if (computationalEVCode) {
+						Reaction reaction = (Reaction) Reaction.load(conn, catalysis.getSlotValue("REACTION"));
+						String EC = null;
+						if (reaction != null) EC = reaction.getEC();
+						if (EC != null && EC.length() > 0) {
+							EC = EC.replace("EC-", "");//Remove the EC- prefix
+							String p = "\\d{1,3}?.\\d{1,3}?.\\d{1,3}?.\\d{1,3}?";//EC numbers are, like IP addresses, 4 numbers of up to 3 digits each, separated by periods. Don't allow partial EC numbers, such as 1.-.-.- or 1.2.3.-
+							Pattern r = Pattern.compile(p);
+							Matcher m = r.matcher(EC);
+							if (m.find()) {
+								ECList.add(EC);//Keep full EC annotations if there was either some computational evidence or no evidence at all (assume computational)
+							} else {
+	//							System.err.println("partial EC? : " + EC);
+							}
+							
+						}
+					} else {
+						System.err.println("Failed to find computational citation for :" + gene.getLocalID());
+					}
 				}
 			}
 		}
